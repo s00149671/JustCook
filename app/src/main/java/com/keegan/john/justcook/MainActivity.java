200fea4201d2acfeb9ae82a1f56618948525b340
s00149671 <s00149671@mail.itsligo.ie>
2017-11-18 03:50:02 +0000

    Initial commit

---
+++ app/src/main/java/com/keegan/john/justcook/MainActivity.java
@@ -0,0 +1,47 @@
+package com.keegan.john.justcook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private Button button;
+
+    public void clickFunctionSubmit(View view) {
+
+        EditText usernameTxt = (EditText) findViewById(R.id.usernameTxt);
+        Log.i("Username", usernameTxt.getText().toString());
+
+        EditText passwordTxt = (EditText) findViewById(R.id.passwordTxt);
+        Log.i("Password", passwordTxt.getText().toString());
+        Toast.makeText(MainActivity.this, "Welcome" + usernameTxt.getText().toString() , Toast.LENGTH_LONG).show();
+    }
+
+    //public void clickFunctionForget(View view) {
+
+        //Intent forgot = new Intent(MainActivity.this, ForgotPassword.class);
+       // startActivity(forgot);
+    //}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    button = (Button) findViewById(R.id.forgotButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openForgotPassword();
+            }
+        });
+    }
+
+    public void openForgotPassword() {
+        Intent forgot = new Intent(MainActivity.this, ForgotPassword.class);
+        startActivity(forgot);
+    }
+}
